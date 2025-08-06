fa1aeddef08e07de97efb50e4df1dc995a9682a9
okada076 <416876pom@gmail.com>
2025-08-06 16:05:28 +0900

    コード修正

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,14 +19,15 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @Tweet = Tweet.find(params[:id])
+    @tweet = Tweet.find(params[:id])
   end
 
  def update
-   tweet = Tweet.find(params[:id])
-   tweet.update(tweet_params)
-   redirect_to root_path
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
  end
+
 
   private
    def tweet_params
