aeed7552a3309c626a6daecacca728d2d0815b7a
okada076 <416876pom@gmail.com>
2025-08-08 12:21:51 +0900

    マイグレーションを編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_07_072614) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_08_031855) do
+  create_table "comments", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", charset: "utf8mb3", force: :cascade do |t|
     t.string "text"
     t.string "image"
